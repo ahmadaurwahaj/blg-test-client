9aaccde5f9b0edb22f888c98e10fb86d4fe34fce
ahmadaurwahaj <ahmadaurwahaj@gmail.com>
2026-03-27 19:16:31 +0500

    fix: root url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
+
 export default defineConfig({
   base: '/free-tools/llm-analysis/',
   plugins: [react(), tailwindcss()],
